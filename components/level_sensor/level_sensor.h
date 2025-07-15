1891038f525947e59c9d26f0fdf95cb58cb1453e
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-07-15 14:10:43 +0100

    new update on level sensor

--- components/level_sensor/level_sensor.h
+++ components/level_sensor/level_sensor.h
@@ -3,26 +3,20 @@
 #include <common_headers.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <rs485.h>
 typedef struct level_sensor_t level_sensor_t;
 
 
 
 typedef struct 
 {
-    int uart_num;
-    int rs485_di_pin; //driver input goes to tx pin 
-    int rs485_ro_pin; // receiver output goes to rx pin
-    int rs485_dir_pin; // de/re for bidirectional communication.
-    int baud_rate;
-   
-    
+    int id;   
+    rs485_config_t rs485;
 }level_sensor_config_t;
 
 
 level_sensor_t* level_sensor_create(const level_sensor_config_t* config);
 error_type_t level_sensor_init(level_sensor_t* level_sensor_obj);
-error_type_t rs485_write(level_sensor_t* level_sensor_obj, const char* data, size_t buffer_size);
-error_type_t rs485_read(level_sensor_t* level_sensor_obj, char* data, size_t buffer_size, int* read_size);
 error_type_t level_sensor_read(level_sensor_t* level_sensor_obj);
 error_type_t level_sensor_destroy(level_sensor_t** level_sensor_obj);
 error_type_t level_sensor_deinit(level_sensor_t* level_sensor_obj);
--- components/level_sensor/src/level_sensor.c
+++ components/level_sensor/src/level_sensor.c
@@ -3,19 +3,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include "driver/uart.h"
-#include "driver/gpio.h"
-#define BUF_SIZE    (127)
-
 
 
 struct level_sensor_t
 {
-    int uart_num;
-    int rs485_di_pin; 
-    int rs485_ro_pin;
-    int rs485_dir_pin;
-    int baud_rate;
+    int id; 
+    rs485_config_t rs485;
     bool activate;
 };
 
@@ -26,11 +19,8 @@ level_sensor_t* level_sensor_create(const level_sensor_config_t* config){
     }
 
     level_sensor_t* level_sensor_obj = (level_sensor_t*)malloc(sizeof(level_sensor_t));
-    level_sensor_obj->uart_num = config->uart_num;
-    level_sensor_obj->rs485_ro_pin = config->rs485_ro_pin;
-    level_sensor_obj->rs485_di_pin = config->rs485_di_pin;
-    level_sensor_obj->rs485_dir_pin = config->rs485_dir_pin;
-    level_sensor_obj->baud_rate = config->baud_rate;
+    level_sensor_obj->id = config->id;
+    level_sensor_obj->rs485 = config->rs485;
     level_sensor_obj->activate = false;
 
     return level_sensor_obj;   
@@ -38,72 +28,17 @@ level_sensor_t* level_sensor_create(const level_sensor_config_t* config){
 
 error_type_t level_sensor_init(level_sensor_t* level_sensor_obj){
     if(level_sensor_obj == NULL)return SYSTEM_NULL_PARAMETER;
-
-    uart_config_t uart_config = {
-    .baud_rate = level_sensor_obj->baud_rate,
-    .data_bits = UART_DATA_8_BITS,
-    .parity = UART_PARITY_DISABLE,
-    .stop_bits = UART_STOP_BITS_1,
-    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-    .source_clk = UART_SCLK_APB,
-   };
-
-    
-    // configure uart pin
-
-    uart_param_config(level_sensor_obj->uart_num, &uart_config);
-    uart_set_pin(level_sensor_obj->uart_num, level_sensor_obj->rs485_di_pin ,level_sensor_obj->rs485_ro_pin, UART_PIN_NO_CHANGE , UART_PIN_NO_CHANGE );
-    uart_driver_install(level_sensor_obj->uart_num, BUF_SIZE*2,0,0,NULL,0);
-
-    //configure gpio pin
-    gpio_set_direction(level_sensor_obj->rs485_dir_pin, GPIO_MODE_OUTPUT);
-
-    gpio_set_level(level_sensor_obj->rs485_dir_pin, 0); // 0 -> LOW, 1 -> HIGH
-                                      // set the rs485 to receiver mode                                  
+    if(!level_sensor_obj->activate)return SYSTEM_INVALID_STATE;
     level_sensor_obj->activate = true;
-
     return SYSTEM_OK;
 }
 
-error_type_t rs485_write(level_sensor_t* level_sensor_obj, const char* data, size_t buffer_size){
-    if (level_sensor_obj == NULL)
-    {
-        return SYSTEM_NULL_PARAMETER;
-    }
 
-    if(!level_sensor_obj->activate){
-        return SYSTEM_INVALID_STATE;
-    }
-    gpio_set_level(level_sensor_obj->rs485_dir_pin, 1);
-    uart_write_bytes(level_sensor_obj->uart_num, data, buffer_size);
-    uart_wait_tx_done(level_sensor_obj->uart_num, portMAX_DELAY);
-    gpio_set_level(level_sensor_obj->rs485_dir_pin, 0);
-    printf("sent data sucessfully");
 
-    return SYSTEM_OK;
- }
-error_type_t rs485_read(level_sensor_t* level_sensor_obj, char* data, size_t buffer_size, int* read_size){
-    if (level_sensor_obj == NULL)
-    {
-        return SYSTEM_NULL_PARAMETER;
-    }
-    
-    *read_size = uart_read_bytes(level_sensor_obj->uart_num, data, buffer_size, pdMS_TO_TICKS(200)); 
-    if (*read_size != -1)
-    {
-         printf("received data sucessfully\n");
-    }
-       
-       
-    
-
-      return SYSTEM_OK;
- }
 
 error_type_t level_sensor_deinit(level_sensor_t* level_sensor_obj){
     if (level_sensor_obj == NULL)return SYSTEM_NULL_PARAMETER;
     level_sensor_obj->activate = false;
-    uart_driver_delete(level_sensor_obj->uart_num);
     return SYSTEM_OK;
     
 }
