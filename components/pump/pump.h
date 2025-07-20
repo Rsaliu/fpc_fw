01ae7e4f1441497932f8b57ee8742779de267e7b
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-07-20 09:53:36 +0100

    [pump] added a print function and test case

--- components/pump/pump.h
+++ components/pump/pump.h
@@ -1,7 +1,7 @@
 #ifndef __PUMP_LIBRARY_H__
 #define __PUMP_LIBRARY_H__
 #include <common_headers.h>
-
+#include <stddef.h>
 
 typedef struct{
     int id;
@@ -26,5 +26,7 @@ error_type_t pump_deinit(pump_t *pump);
 error_type_t pump_destroy(pump_t **pump);
 error_type_t pump_get_state(const pump_t *pump, pump_state_t *state);
 error_type_t pump_get_config(const pump_t *pump, pump_config_t *config);
+error_type_t pump_print_info(pump_t* pump);
+error_type_t pump_print_info_into_buffer(pump_t* pump, char* buffer, const size_t buffer_size);
 
 #endif
