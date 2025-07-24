805e248159e7d78d2bc09001787606956a36bd82
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-07-24 15:21:06 +0100

    [protocol] write test case for the other function

--- components/protocol/test/test_protocol.c
+++ components/protocol/test/test_protocol.c
@@ -12,6 +12,43 @@ void protocoltearDown(void) {
     // Clean up code after each test
 }
 
+TEST_CASE("protocol_test", "test_protocol_gl_a01_write_address"){
+    protocolSetup();
+    uint8_t current_addr= 0x01;
+    uint8_t new_addr = 0x05;
+    uint8_t buffer [8];
+    uint8_t buffer_size = sizeof(buffer);
+    error_type_t err = protocol_gl_a01_write_address(current_addr,new_addr,buffer,buffer_size);
+    TEST_ASSERT_EQUAL(SYSTEM_OK,err);
+    printf("write address was successful\n");
+    protocoltearDown();
+}
+
+TEST_CASE("protocol_test", "test_protocol_gl_a01_read_level"){
+    protocolSetup();
+    uint8_t slave_addr = 0x01;
+    uint8_t buffer[8];
+    int buffer_size = sizeof(buffer);
+    uint8_t payload_size = 0;
+    error_type_t err = protocol_gl_a01_read_level(slave_addr,buffer, buffer_size, &payload_size);
+    TEST_ASSERT_EQUAL(SYSTEM_OK,err);
+    TEST_ASSERT_EQUAL(8, payload_size);
+    printf(" read protocol was successful\n");
+    protocoltearDown();
+}
+
+TEST_CASE("protocol_test", "test_protocol_gl_a01_read_temp"){
+    protocolSetup();
+    uint8_t slave_addr = 0x01;
+    uint8_t buffer[8];
+    uint8_t buffer_size = sizeof(buffer);
+    error_type_t err = protocol_gl_a01_read_temp(slave_addr,buffer,buffer_size);
+    TEST_ASSERT_EQUAL(SYSTEM_OK, err);
+    printf("read temperature protocol is sucessful\n");
+    protocoltearDown();
+
+}
+
 TEST_CASE("protocol_test","test_protocol_gl_a01_interpreter"){
     protocolSetup();
     error_type_t err;
