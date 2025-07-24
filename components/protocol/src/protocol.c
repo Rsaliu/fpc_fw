ad6772c520df440121baccd9a7881db1967d25e6
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-07-24 15:19:31 +0100

    [protocol] change the implementation of the write address

--- components/protocol/src/protocol.c
+++ components/protocol/src/protocol.c
@@ -4,17 +4,17 @@
 #include <string.h>
 #include <stdio.h>
 
-error_type_t protocol_gl_a01_write_address(uint8_t slave_addr, uint8_t buff_size, uint8_t* buffer){
-      if (buff_size!= 8)
+error_type_t protocol_gl_a01_write_address(uint8_t current_addr, uint8_t new_addr, uint8_t* buffer, uint8_t buff_size){
+      if (buff_size < 8)
     {
         return SYSTEM_INVALID_PARAMETER;
     }
-    buffer[0] = slave_addr;
+    buffer[0] = current_addr;
     buffer[1] = 0x06;
     buffer[2] = 0x02;
     buffer[3] = 0x00;
     buffer[4] = 0x00;
-    buffer[5] = 0x05;
+    buffer[5] = new_addr;
     uint16_t crc = MODBUS_CRC16_v3(buffer,6);
     buffer[6] = crc & 0x0FF;
     buffer[7] = (crc >> 8) & 0x0FF; 
@@ -44,8 +44,8 @@ error_type_t protocol_gl_a01_read_level(uint8_t slave_addr, uint8_t* buffer,int 
     return SYSTEM_OK;
 }
 
-error_type_t protocol_gl_a01_read_temp(uint8_t slave_addr, uint8_t buff_size, uint8_t* temp_buffer){
-    if (buff_size != 8)
+error_type_t protocol_gl_a01_read_temp(uint8_t slave_addr, uint8_t* temp_buffer, uint8_t buff_size){
+    if (buff_size < 8)
     {
         return SYSTEM_INVALID_PARAMETER;
     }
