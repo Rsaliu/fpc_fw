1d26da90195dcb5094a4edd736db072c0cb647b7
RSaliu <ridwanriley@gmail.com>
2025-07-24 11:30:46 +0100

    [login-handler] - enhance webserver for handlers

--- components/webserver/webserver.h
+++ components/webserver/webserver.h
@@ -1,8 +1,14 @@
 #ifndef __WEBSERVER_H__
 #define __WEBSERVER_H__
 #include "nvs_flash.h"
+#include "esp_vfs.h"
 #include <common_headers.h>
+#define LOGIN_MAX_USERNAME_LENGTH CONFIG_MAX_USERNAME_LENGTH
+#define LOGIN_MAX_PASSWORD_LENGTH CONFIG_MAX_PASSWORD_LENGTH
 #include "esp_http_server.h"
+
+
+
 typedef struct{
     int port; // Port number for the web server
     char* document_root; // Root directory for serving files
@@ -14,6 +20,14 @@ typedef struct{
     const char *web_partition_label; // Partition label for the web server
 }webserver_config_t;
 
+#define SCRATCH_BUFSIZE (10240)
+//const int SCRATCH_BUFSIZE = 10240; // Size of the scratch buffer for HTTP server
+
+typedef struct rest_server_context {
+    char base_path[ESP_VFS_PATH_MAX + 1];
+    char scratch[SCRATCH_BUFSIZE];
+} rest_server_context_t;
+
 typedef struct webserver_t webserver_t;
 
 webserver_t* webserver_create(const webserver_config_t* config);
@@ -24,5 +38,6 @@ error_type_t webserver_destroy(webserver_t** server);
 error_type_t webserver_deinit(webserver_t* server);
 error_type_t webserver_add_route(webserver_t* server,httpd_uri_t* uri);
 error_type_t webserver_remove_route(webserver_t* server,const char* uri,httpd_method_t method);
+error_type_t webserver_get_scratch_buffer(webserver_t* server, char** buffer, size_t* size);
 
 #endif // __WEBSERVER_H__
