3e529d657f37f5ba0f5e1c9f97c2d07d4255123d
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-08-14 17:32:36 +0100

    [setup_config] comment wifi hotspot unity run test

--- unity-app/main/test_app_main.c
+++ unity-app/main/test_app_main.c
@@ -33,9 +33,7 @@ void app_main(void)
     unity_run_test_by_name("pump_control_unit_test");
     unity_run_test_by_name("relay_test");
     unity_run_test_by_name("webserver_test");
-    unity_run_test_by_name("wifi_hotspot_test");
     unity_run_test_by_name("config_manager_test");
-
     //unity_run_test_by_name("wifi_hotspot_test");
     unity_run_test_by_name("pump_monitor_test");
     unity_run_test_by_name("setup_config_test");
