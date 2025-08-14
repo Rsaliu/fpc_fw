b6cb643f494e227ddf095ecae9e1e08b8a8a2bca
destineddave@gmail.com <info@mainlogix.io>
2025-08-14 18:54:27 +0100

    Json_read and validate

--- unity-app/main/test_app_main.c
+++ unity-app/main/test_app_main.c
@@ -34,9 +34,11 @@ void app_main(void)
     unity_run_test_by_name("relay_test");
     unity_run_test_by_name("webserver_test");
     unity_run_test_by_name("config_manager_test");
-    //unity_run_test_by_name("wifi_hotspot_test");
+    unity_run_test_by_name("wifi_hotspot_test");
     unity_run_test_by_name("pump_monitor_test");
     unity_run_test_by_name("setup_config_test");
+    unity_run_test_by_name("wifi_hotspot_test");
+    unity_run_test_by_name("validate_json_test");
     UNITY_END();
 }
 
