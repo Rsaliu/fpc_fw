71b7cfd1eb62818ddec9df08e7bdfe9954695f7b
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-08-14 14:04:43 +0100

    [setup_config]  added an enum for current sensor interface

--- components/current_sensor/current_sensor.h
+++ components/current_sensor/current_sensor.h
@@ -5,10 +5,20 @@
 
 typedef error_type_t (*current_sensor_reading_callback_t)(void* context, float* current_value);
 
+typedef enum {
+    CURRENT_SENSOR_INTERFACE_I2C,
+    CURRENT_SENSOR_INTERFACE_SPI,
+    CURRENT_SENSOR_INTERFACE_PWM,
+    CURRENT_SENSOR_INTERFACE_ANALOG_VOLTAGE
+}current_sensor_interface_t; 
+
 typedef struct{
     int id; // Unique identifier for the current sensor
     void** context; // Context for the callback, can be used to pass additional data
     current_sensor_reading_callback_t read_current; // Function pointer to read current
+    current_sensor_interface_t interface;
+    char* make;
+    int max_current;
 }current_sensor_config_t;
 
 typedef struct current_sensor_t current_sensor_t;
@@ -18,5 +28,7 @@ error_type_t current_sensor_deinit(current_sensor_t *sensor);
 error_type_t current_sensor_destroy(current_sensor_t **sensor);
 error_type_t current_sensor_get_config(const current_sensor_t *sensor, current_sensor_config_t *config);
 error_type_t current_sensor_get_current_in_amp(const current_sensor_t *sensor, float *current);
+
+current_sensor_interface_t string_to_current_sensor_interface(const char* interface_str);
 
 #endif // __CURRENT_SENSOR_H__
