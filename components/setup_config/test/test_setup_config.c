c0dbad736cddf0c2252cbc818cde447d53e5c214
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-08-14 15:51:50 +0100

    [setup_config] renaming the esp log tag in setup config

--- components/setup_config/test/test_setup_config.c
+++ components/setup_config/test/test_setup_config.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include "unity.h"
 #include "cJSON.h"
-#include "esp_spiffs.h"
 #include "esp_log.h"
 
 static const char* TAG = "SETUP_CONFIG_TEST";
@@ -20,68 +19,66 @@ void setup_config_teardown(void) {
 const char* dummy_json_file() {
    
     static const char* valid_json_data =
-        "{"
-        "\"site_id\": \"SITE123\","
-        "\"device_id\": \"DEVICE456\","
-        "\"pump_control_units\": ["
-        "  {"
-        "    \"tank\": {"
-        "      \"id\": 1,"
-        "      \"capcity_in_liters\": 500.0,"
-        "      \"shape\": \"Cylinder\","
-        "      \"height_in_cm\": 200.0,"
-        "      \"full_level_in_mm\": 1900,"
-        "      \"low_level_in_mm\": 300"
-        "    },"
-        "    \"pump\": {"
-        "      \"id\": 1,"
-        "      \"make\": \"Grundfos\","
-        "      \"power_in_hp\": 1.5"
-        "    },"
-        "    \"tank_monitor\": {"
-        "      \"id\": 1,"
-        "      \"level_sensor_id\": 1,"
-        "      \"tank_id\": 1"
-        "    },"
-        "    \"pump_monitor\": {"
-        "      \"id\": 1,"
-        "      \"current_sensor_id\": 1,"
-        "      \"pump_id\": 1"
-        "    },"
-        "    \"level_sensor\": {"
-        "      \"id\": 1,"
-        "      \"interface\": \"RS485\","
-        "      \"sensor_addr\": 10,"
-        "      \"level_sensor_protocol\": \"GL_A01_PROTOCOL\""
-        "    },"
-        "    \"relay\": {"
-        "      \"id\": 1,"
-        "      \"pin_number\": 25"
-        "    },"
-        "    \"current_sensor\": {"
-        "      \"id\": 1,"
-        "      \"interface\": \"I2C\","
-        "      \"make\": \"ACS712\","
-        "      \"max_current\": 30"
-        "    }"
-        "  }"
-        "]"
-        "}";
-
+    "{"
+    "\"site_id\": \"SITE123\","
+    "\"device_id\": \"DEVICE456\","
+    "\"pump_control_units\": ["
+    "  {"
+    "    \"tank\": {"
+    "      \"id\": 1,"
+    "      \"capcity_in_liters\": 500.0,"
+    "      \"shape\": \"Cylinder\","
+    "      \"height_in_cm\": 200.0,"
+    "      \"full_level_in_mm\": 1900,"
+    "      \"low_level_in_mm\": 300"
+    "    },"
+    "    \"pump\": {"
+    "      \"id\": 1,"
+    "      \"make\": \"Grundfos\","
+    "      \"power_in_hp\": 1.5,"
+    "      \"current_rating\": 2.5"
+    "    },"
+    "    \"tank_monitor\": {"
+    "      \"id\": 1,"
+    "      \"level_sensor_id\": 1,"
+    "      \"tank_id\": 1"
+    "    },"
+    "    \"pump_monitor\": {"
+    "      \"id\": 1,"
+    "      \"current_sensor_id\": 1,"
+    "      \"pump_id\": 1"
+    "    },"
+    "    \"level_sensor\": {"
+    "      \"id\": 1,"
+    "      \"interface\": \"RS485\","
+    "      \"sensor_addr\": 10,"
+    "      \"level_sensor_protocol\": \"GL_A01_PROTOCOL\""
+    "    },"
+    "    \"relay\": {"
+    "      \"id\": 1,"
+    "      \"pin_number\": 25"
+    "    },"
+    "    \"current_sensor\": {"
+    "      \"id\": 1,"
+    "      \"interface\": \"I2C\","
+    "      \"make\": \"ACS712\","
+    "      \"max_current\": 30"
+    "    }"
+    "  }"
+    "]"
+    "}";
     return valid_json_data;
 }
 
 
 TEST_CASE("setup_config_test","test_tank_setup_config"){
     setup_config_Setup();
-
     const char* json_file = dummy_json_file();
     cJSON* root = cJSON_Parse(json_file);
     TEST_ASSERT_NOT_EQUAL(NULL, root);
     error_type_t err = tank_Setup_config(root);
     TEST_ASSERT_EQUAL(SYSTEM_OK, err);
     ESP_LOGI(TAG, "tank_setup_config is sucessful\n");
-    
     setup_config_teardown();
 }
 
