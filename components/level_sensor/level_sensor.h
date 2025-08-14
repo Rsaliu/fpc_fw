098ab3f52ab15446eb2ca6890013cf009ce1b9ae
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-08-14 14:04:43 +0100

    [setup_config]  added an enum for current sensor interface

--- components/level_sensor/level_sensor.h
+++ components/level_sensor/level_sensor.h
@@ -17,13 +17,30 @@ typedef struct
     void* medium_context;
     send_receive_t send_recive;
     protocol_interpreter_t interpreter;
+    level_sensor_interface_t interface;
+    level_sensor_protocol_t level_sensor_protocol;
 }level_sensor_config_t;
+
+typedef enum {
+    LEVEL_SENSOR_INTERFACE_RS485,
+    LEVEL_SENSOR_INTERFACE_UART,
+    LEVEL_SENSOR_INTERFACE_PWM
+
+}level_sensor_interface_t;
+
+typedef enum {
+    GL_A01_PROTOCOL
+
+}level_sensor_protocol_t;
+
 
 
 level_sensor_t* level_sensor_create(level_sensor_config_t config);
 error_type_t level_sensor_init(level_sensor_t* level_sensor_obj);
-error_type_t level_sensor_read(level_sensor_t* level_sensor_obj, uint16_t* read_level);
+error_type_t level_sensor_write(level_sensor_t* level_sensor_obj);
+error_type_t level_sensor_read(level_sensor_t* level_sensor_obj, uint16_t* level);
 error_type_t level_sensor_destroy(level_sensor_t** level_sensor_obj);
 error_type_t level_sensor_deinit(level_sensor_t* level_sensor_obj);
-
+level_sensor_interface_t string_to_level_sensor_interface(const char* interface_str);
+level_sensor_protocol_t string_to_level_sensor_protocol(const char* protocol_str);
 #endif
