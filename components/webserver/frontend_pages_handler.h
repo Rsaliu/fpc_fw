ad2eea13aa10c19eee5b0b5cc4d9b87dc3509dd9
RSaliu <ridwanriley@gmail.com>
2025-08-24 02:49:36 +0100

    [frontend-integration] - adjust frontend handlers for automatic file handling based on file extension.

--- components/webserver/frontend_pages_handler.h
+++ components/webserver/frontend_pages_handler.h
@@ -3,12 +3,6 @@
 #include "esp_http_server.h"
 #include "esp_err.h"
 
-esp_err_t root_page_handler(httpd_req_t *req);
-esp_err_t login_page_handler(httpd_req_t *req);
-esp_err_t register_page_handler(httpd_req_t *req);
-esp_err_t config_page_handler(httpd_req_t *req);
-
-
-
+esp_err_t rest_common_get_handler(httpd_req_t *req);
 
 #endif // __FRONT_END_HANDLER_H__
