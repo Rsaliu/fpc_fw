803a2fb4d88d45f455481ec2110d52c0794c5cfe
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-09-05 16:10:26 +0100

    [setup_config_button] define event flag , set an isr bit and create event group for setup_config_button

--- components/setup_config_button/setup_config_button.h
+++ components/setup_config_button/setup_config_button.h
@@ -2,17 +2,18 @@
 #define __SETUP_CONFIG_BUTTON__
 #include <common_headers.h>
 #include <stdbool.h>
+#include <stdint.h>
 
  
 typedef struct 
 {
 
-    int button_pin_number;
+    uint8_t button_pin_number;
     bool setup_config_button_mode;
 
 }setup_config_button_config_t;
-
 void setup_config_button_task(void* Pvparameter);
+void setup_config_button_init(setup_config_button_config_t* config);
 
 
 #endif
--- components/setup_config_button/src/setup_config_button.c
+++ components/setup_config_button/src/setup_config_button.c
@@ -3,15 +3,63 @@
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
+#include "freertos/event_groups.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
 
-static QueueHandle_t setup_config_button_event = NULL;
+#define BUTTON_FLAG_BIT0 (1 << 0)
 
-static void IRAM_ATTER setup_config_button_isr_handler(void*arg){
-    setup_config_button_config_t* config = (setup_config_button_config_t*)arg;
-    int pin_num = config->button_pin_number;
+static EventGroupHandle_t button_event;
+static const char*TAG = "SETUP_CONFIG_BUTTON";
 
-    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    xQueueSendFromISR(setup_config_button_event,&pin_num, xHigherPriorityTaskWoken);
+static void dummy_setup_config_button(){
+    ESP_LOGI(TAG, "FPC is in configuration mode");
+
 }
+
+static void IRAM_ATTR setup_config_button_isr_handler(void*arg){
+    setup_config_button_config_t* config = (setup_config_button_config_t*)arg;
+    config->setup_config_button_mode = true;
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xEventGroupSetBitsFromISR(button_event, BUTTON_FLAG_BIT0, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+    
+}
+
+void setup_config_button_task(void* Pvparameter){
+    uint8_t button_pin_number;
+    EventBits_t event_bit;
+    while (1)
+    {
+        event_bit = xEventGroupWaitBits(button_event, BUTTON_FLAG_BIT0,pdTRUE,pdFALSE,portMAX_DELAY);
+        if ((event_bit & BUTTON_FLAG_BIT0) =! 0)
+        {
+            ESP_LOGI(TAG, "button pressed !!!\n");
+             dummy_setup_config_button();
+        }
+        
+       
+    }
+    
+
+}
+
+void setup_config_button_init(setup_config_button_config_t* config){
+    button_event = xEventGroupCreate();
+    if (button_event == NULL)
+    {
+        ESP_LOGE(TAG, "failed to create event group\n");
+        return SYSTEM_INVALID_PARAMETER;
+    }
+    
+    gpio_config_t  io_config = {};
+    io_config.intr_type = GPIO_INTR_POSEDGE;
+    io_config.mode = GPIO_MODE_INPUT;
+    io_config.pin_bit_mask = (1ULL << config->button_pin_number);
+    io_config.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpio_config(&io_config);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(config->button_pin_number,setup_config_button_isr_handler,(void*) config);
+}
+
