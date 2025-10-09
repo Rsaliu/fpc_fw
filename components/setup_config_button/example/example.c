1cca43b6237c7b82588d52933d6b4a30909a6a9d
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-10-09 09:13:13 +0100

    [setup_config_button_two] added  functionality to start webserver and stop other task vice vera in response to button pressed or release.

--- components/setup_config_button/example/example.c
+++ components/setup_config_button/example/example.c
@@ -5,22 +5,31 @@
 #include <common_headers.h>
 #include "esp_log.h"
 
-static const char*TAG = "SETUP_CONFIG_BUTTON";
+tatic const char*TAG = "SETUP_CONFIG_BUTTON";
 
-static error_type_t dummy_setup_config_button(){
-    ESP_LOGI(TAG, "FPC is in configuration mode");
-    return SYSTEM_OK;
-
+static void dummy_webserver(void*pvParameter){
+    for (;;)
+    {
+        ESP_LOGI(TAG, "FPC is in configuration mode");
+        vTaskDelay(1000);
+    }
+    
+    
 }
-
-setup_config_button_config_t config = {
+ TaskHandle_t other_task_handle;
+setup_config_button_config_t button_config = {
     .button_pin_number = 4,
-    .config_button = dummy_setup_config_button
+    .other_task = NULL,
+    .webserver = dummy_webserver
 };
 
 void app_main(void)
 {
+
     printf("Hello world!\n");
-    setup_config_button_init(&config);
-    xTaskCreate(&setup_config_button_task, "setup_config_button", 2048, &config, 1, NULL);
-}    
+
+        
+         setup_config_button_init(&button_config);
+         setup_config_button_two_event(&button_config);
+
+}         
