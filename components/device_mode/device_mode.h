72d370502640f5422753745fc6d7ce66e09a92d6
oladeinde maryam <mariamoladeinde19@gmail.com>
2025-10-22 10:21:40 +0100

    [device_mode] change the file name to device_mode

---
+++ components/device_mode/device_mode.h
@@ -0,0 +1,24 @@
+#ifndef __DEVICE_MODE_H__
+#define __DEVICE_MODE_H__
+#include <stdbool.h>
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <common_headers.h>
+
+typedef void(*websever_task_t)(void*pvParameter);
+typedef void(*task_handler_t)(void);
+
+typedef struct 
+{
+
+    task_handler_t other_task;
+    uint8_t button_pin_number;
+    websever_task_t webserver;
+}device_mode_config_t;
+
+error_type_t device_mode_event(device_mode_config_t*config);
+void device_mode_init(device_mode_config_t* config);
+
+ 
+#endif
---
+++ components/device_mode/src/device_mode.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <device_mode.h>
+#include "freertos/event_groups.h"
+#include "driver/gpio.h"
+#include "esp_intr_types.h"
+#include "esp_log.h"
+#include "example.h"
+
+static const char*BUTTON_TAG = "SETUP_CONFIG_BUTTON";
+
+static void IRAM_ATTR device_mode_isr_handler(void*arg){
+    BaseType_t xHigherPriorityTaskWoken;
+    xHigherPriorityTaskWoken = pdFALSE;
+    esp_restart();
+    if ( xHigherPriorityTaskWoken )
+    {
+        portYIELD_FROM_ISR();
+    }     
+}
+
+error_type_t device_mode_event(device_mode_config_t*config){
+    int button_level = gpio_get_level(config->button_pin_number);
+    if (button_level == 0)
+    {
+        ESP_LOGI(BUTTON_TAG, "button pressed: start webserver\n.");
+        config->webserver();
+        //xTaskCreate(config->webserver, "websever_task",4096,NULL,1,NULL);
+        
+    }else
+    {
+
+        ESP_LOGI(BUTTON_TAG, "button release: continue other task");
+        config->other_task();
+        
+    }
+
+    return SYSTEM_OK;
+}
+
+void setup_config_button_init(device_mode_config_t* config){
+    gpio_config_t  io_config = {};
+    io_config.intr_type = GPIO_INTR_ANYEDGE;
+    io_config.mode = GPIO_MODE_INPUT;
+    io_config.pin_bit_mask = (1ULL << config->button_pin_number);
+    io_config.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpio_config(&io_config);
+
+    //ESP_INTR_FLAG_LEVEL3 use to set the isr priority level
+    gpio_install_isr_service(ESP_INTR_FLAG_LEVEL3);
+    gpio_isr_handler_add(config->button_pin_number,device_mode_isr_handler,(void*) config);
+}
+
